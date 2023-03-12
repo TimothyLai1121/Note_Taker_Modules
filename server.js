55e718d350c2816beb45d3cd6d2699b6a1247b17
Timothy Lai <timothylai1121@gmail.com>
2023-03-12 14:49:21 -0400

    adding server.js
    adding server.js and working on Insomnia for get/post/

---
+++ server.js
@@ -0,0 +1,77 @@
+// adding npm i express //
+const express = require('express');
+
+// adding path //
+const path = require('path');
+
+// adding fs //
+const fs = require('fs');
+
+// adding app = express //
+const app = express();
+
+// adding PORT //
+// process.env.PORT is for Heroku //
+// 3000 is for local //
+// Using || means if process.env.PORT is not available, use 3000 //
+const PORT = process.env.PORT || 3000;
+
+// adding middleware //
+// urlencoded is for parsing data from html forms = x-www-form-urlencoded //
+app.use(express.urlencoded({ extended: true }));
+// json is for parsing data from html forms = json //
+app.use(express.json());
+
+// setting up public folder as a static folder //
+app.use(express.static(path.join(__dirname, 'public')));
+
+// setting up the routes for note pages //
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/notes.html'));
+});
+
+// setting up the routes for index page //
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+// setting up the route for getting the saved note db/db.json //
+app.get('/api/notes', (req, res) => {
+    fs.Readfile(path.join(__dirname, 'db/db.json'), 'utf8', (err, data) => {
+        if (err) throw err;
+        res.json(JSON.parse(data));
+    });
+});
+
+// setting up the route for posting the saved note db/db.json //
+app.post('/api/notes', (req, res) => {
+    fs.readFile(path.join(__dirname, 'db/db.json'), 'utf8', (err, data) => {
+        if (err) throw err;
+        const notes = JSON.parse(data);
+        const newNote = req.body;
+        notes.push(newNote);
+        fs.writeFile(path.join(__dirname, 'db/db.json'), JSON.stringify(notes), err => {
+            if (err) throw err;
+            res.json(newNote);
+        });
+    });
+});
+
+// setting up the route for deleting the saved note db/db.json //
+app.delete('/api/notes/:id', (req, res) => {
+    fs.readFile(path.join(__dirname, 'db/db.json'), 'utf8', (err, data) => {
+        if (err) throw err;
+        const notes = JSON.parse(data);
+        const newNotes = notes.filter(note => note.id !== req.params.id);
+        fs.writeFile(path.join(__dirname, 'db/db.json'), JSON.stringify(newNotes), err => {
+            if (err) throw err;
+            res.json(newNotes);
+        });
+    });
+});
+
+// starting the server //
+app.listen(PORT, () => {
+    console.log(`App listening on PORT ${PORT}`);
+});
+
