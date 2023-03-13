ecf63aa678778636232fdd252ee230b7c182a36f
Timothylai1121 <timothylai1121@gmail.com>
2023-03-12 21:52:25 -0400

    adding paper_texture.js

---
+++ public/assets/js/paper_texture.js
@@ -0,0 +1,7 @@
+const paperSelect = document.querySelector("#paper-select");
+const textarea = document.querySelector(".note-textarea");
+
+paperSelect.addEventListener("change", function() {
+  const selectedPaper = paperSelect.value;
+  textarea.style.backgroundImage = `url(${selectedPaper})`;
+});
