cbdc281d5e0a229d4a5509f0d5ae650b3a724c3f
Timothylai1121 <timothylai1121@gmail.com>
2023-03-12 22:25:06 -0400

    testing quill

--- public/assets/js/paper_texture.js
+++ public/assets/js/paper_texture.js
@@ -5,3 +5,20 @@ paperSelect.addEventListener("change", function() {
   const selectedPaper = paperSelect.value;
   textarea.style.backgroundImage = `url(${selectedPaper})`;
 });
+
+
+// adding Quill //
+
+var quill = new Quill('.note-textarea', {
+  modules: {
+    toolbar: [
+      [{ header: [1, 2, false] }],
+      ['bold', 'italic', 'underline'],
+      ['image', 'code-block']
+    ]
+  },
+  placeholder: 'Compose an epic...',
+  theme: 'snow'
+});
+
+var content = quill.root.innerHTML;
